912957e63f2314a93cc30fcb491f5943bce4b6bb
Christian Gibbs <christian@cjgsolutions.com>
2017-07-12 13:58:39 +0100

    Google Optimize added

--- components/head.php
+++ components/head.php
@@ -14,6 +14,24 @@
     <script src="../../assets/js/actions.js" charset="utf-8"></script>
     <!-- FontAwesome -->
     <link rel="stylesheet" href="../../assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    
+    <style>.async-hide { opacity: 0 !important} </style>
+    <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+    })(window,document.documentElement,'async-hide','dataLayer',4000,
+    {'GTM-M7H6DCJ':true});</script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-13062331-2', 'auto');
+      ga('require', 'GTM-M7H6DCJ');
+      ga('send', 'pageview');
+    </script>
+
 
     <style media="screen">
       body {margin: 0px; padding: 0px; background-color: #f5f5f5; font-family: lato;}
@@ -74,7 +92,7 @@
             </script>
 
             <li>
-              <a href="tel:01138876270" title="Phone">
+              <a href="tel:01138876270" title="Phone" style="font-size: 40px;">
                 <i class="fa fa-phone" aria-hidden="true"></i> PHONE - CALL 0113 8876270
               </a>
             </li>
