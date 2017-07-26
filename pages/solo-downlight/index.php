d64cf06463b6a4cdda0b6fb7d4bde553d25021e9
Christian Gibbs <christian@cjgsolutions.com>
2017-07-26 15:25:31 +0100

    solo-downlight landing created

---
+++ pages/solo-downlight/index.php
@@ -0,0 +1,327 @@
+<?php
+include('../../components/head.php');
+?>
+
+<script src="assets/parallax.js-1.4.2/parallax.min.js"></script>
+
+<!-- <div class="parallax-container" data-parallax="scroll" data-position="middle" data-bleed="50" data-image-src="images/banner.png" data-natural-width="1400" data-natural-height="900" style="height: 400px;"></div> -->
+<style media="screen">
+  .colour-temps .col, .col-m {margin: 0% 0 0% 1.5%; text-align: center;}
+  .downlight-page h1, .downlight-page h2, .downlight-page h3, .downlight-page p {color: #333;}
+  .solo-info {text-align: center;}
+  .solo-info h2 {font-size: 18px; color: #000000;}
+  .solo-info h3 {font-size: 16px; color: #333333;}
+  .solo-info h4, .colour-temps h4 {font-size: 16px; color: #555555;}
+  .info-panel-short {display: none; text-align: center;}
+  @media screen and (max-width: 1300px) {
+    .info-panel-long {display: none;}
+    .info-panel-short {display: block;}
+    .image-panel {width: 100%;}
+  }
+  .tblSpec tr:nth-child(even) {
+    background-color: #dfeaef;
+  }
+
+  .tblSpec {width: 100%;}
+  .tblSpec tr td {width: 50%; text-align: left;}
+  .tblSpec tr td:nth-child(1) {font-weight: bold;}
+</style>
+<div class="row downlight-page">
+  <div class="col col-1" style="background-color: #009FE3">
+    <h2 style="padding:10px; color: #fff;">ALL-IN-ONE LED DWNLIGHT 10W</h2>
+  </div>
+  <div class="col col-1" style="background-color: #fff;">
+    <div class="col col-p-6 image-panel"  style="background-color: #addaec;">
+      <img src="images/banner.png" alt="" style="width: 100%;">
+      <div class="col col-1" style="background-color: #009FE3">
+        <h2 style="padding:10px; color: #fff;">Call Us For Any Enquieriers: 0113 887 6270</h2>
+      </div>
+      <div class="col col-2">
+        <div class="solo-info">
+          <div class="panel-padding">
+            <h2 style="color: #333;">BEST PLACE TO BUY ONLINE</h2>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> Best Price Available Online</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> Bulb Prices Available</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> Will NOT Be Beaten On Price</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> Huge Stock Available</b>
+            </h3><br />
+          </div>
+        </div>
+        <div class="solo-info">
+          <div class="panel-padding">
+            <h2 style="color: #333;">WINNING ORIGINAL FEATURES</h2>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> SAME BRIGHT OUTPUT</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> SAME 5-YEAR WARRANTY</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> SAME APPEARANCE</b>
+            </h3>
+            <h4>Switch is behind removable fascia</h4><br />
+          </div>
+        </div>
+      </div>
+      <div class="col col-2">
+        <div class="solo-info">
+          <div class="panel-padding">
+            <h2 style="color: #333;">NEW &amp; IMPROVED DESIGN</h2>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> EASIER COLOUR CHANGE</b>
+            </h3>
+            <h4>Switch is behind removable fascia</h4>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> EASIER TO INSTALL</b>
+            </h3>
+            <h4>Fits in 90mm void height<br />
+            Driver on flexible connection</h4>
+          </div>
+        </div>
+        <div class="solo-info">
+          <div class="panel-padding">
+            <h2 style="color: #333;">1 DOWNLIGHT</h2>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> DIMMABLE</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> FIRE RESISTANT 90 MINUTES</b>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> WATER RESISTANT</b>
+              <h4>IP65 Fascia</h4>
+            </h3>
+            <h3>
+              <b><i class="fa fa-check" style="color: #009FE3"></i> 3-HOUR EMERGENCY VERSION AVAILABLE</b>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <button type="submit" title="Search" class="button" style="width: 100%; margin-top:10px; height: 80px;">
+          <h1 style="color: #fff;"><i class="fa fa-shopping-cart" style="color: #fff"></i> CLICK HERE TO BUY YOUR SOLO LED DOWNLIGHT</h1>
+      </button>
+    </div>
+    <div class="col col-p-6">
+      <div class="panel panel-padding">
+        <div class="panel-head" style="background-color: #009FE3">
+          <h2 style="padding:10px; color: #fff;">3 COLOURS, 3 FASCIAS, 1 DOWNLIGHT</h2>
+        </div>
+        <div class="panel-body colour-temps" style="overflow: hidden;">
+          <div class="col col-3">
+            <img src="images/colour-warm.png" alt="" style="width: 100%; padding-bottom: 5px;">
+            <h3>3000K WARM WHITE</h3>
+            <h4>Recommended for home &amp; hospitality</h4><br />
+          </div>
+          <div class="col col-3">
+            <img src="images/colour-cool.png" alt="" style="width: 100%; padding-bottom: 5px;">
+            <h3>4000K COOL WHITE</h3>
+            <h4>Recommended for lobby, corridor &amp; washwoom</h4><br />
+          </div>
+          <div class="col col-3">
+            <img src="images/colour-daylight.png" alt="" style="width: 100%; padding-bottom: 5px;">
+            <h3>6000K DAYLIGHT WHITE</h3>
+            <h4>Recommended for office &amp; workshop</h4><br />
+          </div>
+        </div>
+      </div>
+
+      <table class="tab-compare" cellspacing="5">
+          <tr>
+              <th></th>
+              <th>MyLights</th>
+              <th>Lamp Shop Online</th>
+              <th>Click 4 Electrics</th>
+          </tr>
+          <tr>
+              <td>10W SOLO</td>
+              <td>&pound;18.60</td>
+              <td>&pound;18.54</td>
+              <td>&pound;18.72</td>
+          </tr>
+      </table>
+
+      <div class="col col-2">
+        <div class="panel panel-padding">
+          <div class="panel-head" style="background-color: #009FE3">
+            <h2 style="padding:10px; color: #fff;">3 FASCIAS (Steel, Chrome &amp; White)</h2>
+          </div>
+          <div class="panel-bodys">
+            <img src="images/fascias.png" alt="" style="width: 100%; padding-top: 20px;">
+          </div>
+        </div>
+      </div>
+      <div class="col col-2 col-m">
+        <div class="panel panel-padding">
+          <div class="panel-head" style="background-color: #009FE3">
+            <h2 style="padding:10px; color: #fff;">SPACE REQUIREMENT</h2>
+          </div>
+          <div class="panel-bodys">
+            <img src="images/space-requirement.png" alt="" style="width: 100%; padding-top: 20px;">
+          </div>
+        </div>
+      </div>
+  </div>
+  <div class="col col-1">
+
+    <div class="col col-p-9 col-m">
+      <div class="panel panel-padding">
+        <div class="col col-1" style="background-color: #009FE3">
+          <h2 style="padding:10px; color: #fff;">ALL-IN-ONE LED DWNLIGHT 10W</h2>
+        </div>
+        <div class="col col-2 col-m">
+          <table class="tblSpec" cellspacing="0" cellpadding="5">
+            <tr>
+              <td>Item</td>
+              <td>833570</td>
+            </tr>
+            <tr>
+              <td>Quantity</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Light Colour</td>
+              <td>3,000K (WW) / 4,000K (CW) / 6,000K (DL)</td>
+            </tr>
+            <tr>
+              <td>Ra, Consistency</td>
+              <td>Ra80, 6 SDCM</td>
+            </tr>
+            <tr>
+              <td>Luminous flux</td>
+              <td>615lm (WW) / 740lm (CW) / 649lm (DL)</td>
+            </tr>
+            <tr>
+              <td>Beam Angle, Light Intensity</td>
+              <td>40&deg;, 2185 cd</td>
+            </tr>
+            <tr>
+              <td>Accent Lighting</td>
+              <td>Yes</td>
+            </tr>
+            <tr>
+              <td>Warm Up Time</td>
+              <td>Instant</td>
+            </tr>
+            <tr>
+              <td>Dimmable</td>
+              <td>Yes (Max.20 fittings)</td>
+            </tr>
+            <tr>
+              <td>Size</td>
+              <td>55mm x 85mm x 85mm (Excluding driver)</td>
+            </tr>
+            <tr>
+              <td>Power Input</td>
+              <td>10W at 230VAC 50-60Hz</td>
+            </tr>
+            <tr>
+              <td>Power Factor &amp; Current</td>
+              <td>0.9, 285mA</td>
+            </tr>
+          </table>
+        </div>
+        <div class="col col-2 col-m">
+          <table class="tblSpec" cellspacing="0" cellpadding="5">
+            <tr>
+              <td>Lamp Type</td>
+              <td>COB</td>
+            </tr>
+            <tr>
+              <td>Fire Resistance</td>
+              <td>BS476-22 90 minutes</td>
+            </tr>
+            <tr>
+              <td>Working Temperature</td>
+              <td>-30&deg;c, +35&deg;c</td>
+            </tr>
+            <tr>
+              <td>Construction</td>
+              <td>Aluminium heat sink, steel fascia</td>
+            </tr>
+            <tr>
+              <td>IP Rating</td>
+              <td>IP65 Fascia only</td>
+            </tr>
+            <tr>
+              <td>Energy Efficiency Index</td>
+              <td>A+</td>
+            </tr>
+            <tr>
+              <td>Energy Consumption</td>
+              <td>10 kWh / 1000 h</td>
+            </tr>
+            <tr>
+              <td>Lifetime</td>
+              <td>50,000 h</td>
+            </tr>
+            <tr>
+              <td>Building Regs - Part L</td>
+              <td>Yes</td>
+            </tr>
+            <tr>
+              <td>Warranty &amp; LMF at EoL</td>
+              <td>5 years</td>
+            </tr>
+            <tr>
+              <td>Switching Cycles</td>
+              <td>100,000</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="col col-p-3">
+      <div class="panel panel-padding">
+        <img src="images/icons.png" alt="" style="width: 90%; padding: 10px;">
+      </div>
+    </div>
+  </div>
+  <div class="col col-1">
+    <div class="panel panel-padding">
+      <div class="panel-head" style="background-color: #009FE3">
+        <h2 style="padding:10px; color: #fff;">ALL-IN-ONE LED DWNLIGHT 10W</h2>
+      </div>
+      <div class="panel-body"  style="overflow: hidden;">
+        <div class="col col-8 col-m" style="text-align: center;">
+          <img src="images/step1.png" alt="">
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <p>Make the desired position and carefully cut out. Space requirements on base of carton.</p>
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <img src="images/step2.png" alt="">
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <p>Connect wires using the plug and play connectors.</p>
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <img src="images/step3.png" alt="">
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <p>Unscrew bezel; move switch to select light colour: warm, white, cool white, daylight.</p>
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <img src="images/step4.png" alt="">
+        </div>
+        <div class="col col-8 col-m" style="text-align: center;">
+          <p>...Optionally affix a different magnetic bezel from the assortment in the carton.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+</div>
+
+<?php
+ include('../../components/footer.php');
+?>
