98d954d7b67e859e1d5cb952983bb4d683b13b69
Christian Gibbs <christian@cjgsolutions.com>
2017-08-17 14:50:12 +0100

    spelling

--- pages/tridonic-control-gear/index.php
+++ pages/tridonic-control-gear/index.php
@@ -122,7 +122,7 @@
               <div class="col col-1">
                 <div class="panel panel-default">
                   <div class="panel-heading" style="background-color: #119B3C;">
-                    <h4 style="padding: 10px;">Emergency Combo Balasts</h4>
+                    <h4 style="padding: 10px;">Emergency Combo Ballasts</h4>
                   </div>
                   <div class="panel-body">
                     <img src="https://www.lampshoponline.com/media/catalog/category/tridonic-pc-combo__63287.jpg" alt="">
@@ -209,8 +209,74 @@
   </div>
 </div>
 
+<style media="screen">
+  footer .col {margin: 0% 0 0% 1.5%;}
 
+</style>
+<footer>
+  <div class="row">
+    <div class="col col-3">
+      <div class="panel panel-padding" style="margin-top: 0px; padding-top: 0px;">
+        <div class="panel-head" style="background-color: #808080;">
+          <h2 style="padding: 10px;"><i class="fa fa-check" aria-hidden="true"></i> Recommended Brand    <i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></h2>
+        </div>
+        <div class="panel-body" style="min-height: 60px; font-size: 16px;">
+          <?php echo $panel; ?>
+        </div>
+      </div>
+    </div>
+    <div class="col col-3">
+      <div class="panel panel-padding" style="margin-top: 0px; padding-top: 0px;">
+        <div class="panel-head" style="background-color: #FD727A;">
+          <h2 style="padding: 10px;"><i class="fa fa-truck" aria-hidden="true"></i> FREE DELIVEY on orders over &pound;40</h2>
+        </div>
+        <div class="panel-body" style="min-height: 60px; font-size: 16px;">
+          If you spend more than £40 we will happily deliver your order for free.
+          *Mainland UK only.
+        </div>
+      </div>
+    </div>
+    <div class="col col-3">
+      <div class="panel panel-padding" style="margin-top: 0px; padding-top: 0px;">
+        <div class="panel-head" style="background-color: #209F96;">
+          <h2 style="padding: 10px;"><i class="fa fa-calendar" aria-hidden="true"></i> 30 DAYS HASSLE FREE RETURN</h2>
+        </div>
+        <div class="panel-body" style="min-height: 60px; font-size: 16px;">
+          Bought the wrong item? We'll allow up to 30 days to return with your money back
+        </div>
+      </div>
+    </div>
+  </div>
 
- <?php
-   include('../../components/footer.php');
-  ?>
+  <div class="row">
+    <div class="col col-1">
+      <div class="panel panel-padding">
+        <div class="panel-head">
+          <div class="panel-head" style="background-color: #808080;">
+            <h2 style="padding: 10px;"><i class="fa fa-smile-o" aria-hidden="true"></i> See what our customers say about us</h2>
+          </div>
+        </div>
+        <div class="panel-body">
+          Check out our reviews on reviews.co.uk below.
+          This is an independent website so we have no control over what is written good or bad.
+          Just honest feedback from real customers.
+
+          <script src="https://widget.reviews.co.uk/vertical/dist.js"></script>
+          <div id="full-page-widget" style="width:100%;"></div>
+          <script>
+          verticalWidget('full-page-widget',{
+            store: 'lampshoponline',
+            primaryClr: '#11CD6B',
+            layout:'fullWidth',
+            height: 500,
+            numReviews: 21
+          });
+          </script>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<style media="screen">
+  .review_text {font-size: 16px;}
+</style>
