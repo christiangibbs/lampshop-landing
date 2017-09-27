fe52b3ea4cc101d04bcd41dbe387ad03b59c42d8
Christian Gibbs <christian@cjgsolutions.com>
2017-09-27 10:14:38 +0100

    fluorescent page v2 added

---
+++ pages/fluorescent-tubes-v2/index.php
@@ -0,0 +1,216 @@
+<?php
+  $page = 'Fluorescent Tubes';
+  include('../../components/head-minimal.php');
+  $panel = "We recommend any of our Venture products as they are a premium brand and the highest quality.";
+ ?>
+<style media="screen">
+  .product img {max-height: 150px; min-height: 150px;}
+  .product .thumbnail {margin-bottom:0px; border-radius: 0px;}
+  .product h3 {margin: 0px;}
+  .product .panel {border-radius:0px;}
+  .products .product {height: auto;}
+</style>
+
+<style media="screen">
+  .usp {text-align: center; color: #ffffff;}
+  .usp h1 {font-size: 22px; font-weight: bold; margin: 10px;}
+</style>
+ <body>
+
+   <div class="row" style="margin: 0px;
+   max-width: 100%;
+   width: 100%;
+   background: #119b3c;
+   background-image: linear-gradient(#57e073,#119b3c);
+   background-size: 110px 110px;
+   background-repeat: repeat-x;">
+     <div class="col-sm-2">
+       <a href="https://www.lampshoponline.com/" class="logo-href">
+         <img class="logo" src="https://www.lampshoponline.com/skin/frontend/rwd/lampshop/images/logo.png" alt="Logo">
+       </a>
+     </div>
+   </div>
+
+ <div class="col-sm-4 usp" style="background-color: #E28413;">
+   <h1><i class="fa fa-trophy" aria-hidden="true"></i> Largest Online Stockist</h1>
+ </div>
+ <div class="col-sm-4 usp" style="background-color: #FFBA08;">
+   <h1><i class="fa fa-gbp" aria-hidden="true"></i> Best Price Available Online</h1>
+ </div>
+ <div class="col-sm-4 usp" style="background-color: #DD1C1A;">
+   <h1><i class="fa fa-certificate" aria-hidden="true"></i> All major brands stocked</h1>
+ </div>
+ <div class="col-sm-12" style="background-color: #e0e0e0;">
+   <h1 style="position: relative; color: #333;">Normal Fluorescent Tubes</h1>
+ </div>
+
+<div class="col-sm-12 products" style="text-align: center; margin-top: -2px;">
+  <div class="col-sm-3 product">
+   <div class="panel panel-default">
+       <a href="https://www.lampshoponline.com/fluorescent-tubes/t5-tubes/miniature-t5-tubes.html" class="thumbnail">
+         <img src="https://www.lampshoponline.com/media/catalog/category/longer_t5_copy.jpg" alt="T5 Mini">
+       </a>
+     <div class="panel-body">
+       <h3>T5 Mini</h3>
+     </div><!-- panel-body -->
+   </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/t5-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/longer_t5_copy.jpg" alt="T5 Standard">
+        </a>
+      <div class="panel-body">
+        <h3>T5 Standard</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+
+   <div class="col-sm-3 product">
+     <div class="panel panel-default">
+         <a href="https://www.lampshoponline.com/fluorescent-tubes/t8-tubes.html" class="thumbnail">
+           <img src="https://www.lampshoponline.com/media/catalog/category/t8_tube.jpg" alt="T8 Tubes">
+         </a>
+       <div class="panel-body">
+         <h3>T8 Tubes</h3>
+       </div><!-- panel-body -->
+     </div><!-- panel panel-default -->
+   </div><!-- col-sm-2 product w16 branded pin2 -->
+
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/t12-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/t12_longer_web_pics_copy_copy.jpg" alt="T12 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>T12 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+</div>
+<div class="col-sm-12" style="background-color: #e0e0e0;">
+  <h1 style="position: relative; color: #333;">Specialist Tubes</h1>
+</div>
+<div class="col-sm-12 products" style="text-align: center; margin-top: -2px;">
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/shatterproof-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/shatterproof_1.jpg" alt="Shatterproof Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Shatterproof Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/full-spectrum-activa-t8-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/blacklight-tube__65396.jpg" alt="Full Spectrum Active T8 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Full Spectrum Active T8 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/gourmet-t8-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/gourmet_tube.jpg" alt="Gourmet T8 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Gourmet T8 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/grolux-t8-fluorescent-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/grolux_t8.jpg" alt="Grolux T8 Fluorescent Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Grolux T8 Fluorescent Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/grolux-t5-fluorescent-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/grow_T5.jpg" alt="Grolux T5 Fluorescent Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Grolux T5 Fluorescent Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/t8-germicidal-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/germicidal_t8_tube_.jpg" alt="T8 Germicidal Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>T8 Germicidal Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/t5-germicidal-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/t5_germicidal_tube__87976.1310466869.800.800_category.jpg" alt="T5 Germicidal Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>T5 Germicidal Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/blacklight-insectocutor-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/blacklight-tube__43577.jpg" alt="Blacklight Insectocutor Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Blacklight Insectocutor Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/blacklight-blue-t8-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/t8-blb__53801.jpg" alt="Blacklight Blue T8 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Blacklight Blue T8 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/blacklight-blue-t5-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/t5-blb__87623.jpg" alt="Blacklight Blue T5 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Blacklight Blue T5 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/sad-lamps.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/SAD_t8_tube_.jpg" alt="SAD Lamps">
+        </a>
+      <div class="panel-body">
+        <h3>SAD Lamps</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+  <div class="col-sm-3 product">
+    <div class="panel panel-default">
+        <a href="https://www.lampshoponline.com/fluorescent-tubes/special-purpose-tubes/coloured-t8-tubes.html" class="thumbnail">
+          <img src="https://www.lampshoponline.com/media/catalog/category/coloured_T8_tubes_.jpg" alt="Coloured T8 Tubes">
+        </a>
+      <div class="panel-body">
+        <h3>Coloured T8 Tubes</h3>
+      </div><!-- panel-body -->
+    </div><!-- panel panel-default -->
+  </div><!-- col-sm-2 product w16 branded pin2 -->
+</div>
