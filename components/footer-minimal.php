bdd4c1940d9b144f74ea4703d162c714b7759d89
Christian Gibbs <christian@cjgsolutions.com>
2017-10-24 15:20:23 +0100

    led fittings finished

---
+++ components/footer-minimal.php
@@ -0,0 +1,52 @@
+<style media="screen">
+  footer .panel {border-radius: 0px; }
+</style>
+<footer>
+  <div class="col-sm-12">
+    <div class="col-sm-6">
+      <div class="col-sm-12">
+        <div class="panel panel-default">
+          <div class="panel-heading" style="background-color: #808080; background-image: none; color: #ffffff;">
+            Recommended Lamps
+          </div>
+          <div class="panel-body">
+            We recommend any of our <?php echo $page; ?> as they are all premium brands and the highest quality.
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <div class="panel panel-default">
+          <div class="panel-heading" style="background-color: #FD727A; background-image: none; color: #ffffff;">
+            30 DAYS HASSLE FREE RETURN
+          </div>
+          <div class="panel-body">
+            Bought the wrong item? We'll allow up to 30 days to return with your money back
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <div class="panel panel-default">
+          <div class="panel-heading" style="background-color: #209F96; background-image: none; color: #ffffff;">
+            FREE DELIVERY On Orders over &pound;40
+          </div>
+          <div class="panel-body">
+            If you spend more than &pound;40 we will happily deliver your order for free. *Mainland UK only.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <script src="https://widget.reviews.co.uk/vertical/dist.js"></script>
+      <div id="full-page-widget" style="width:100%;"></div>
+      <script>
+      verticalWidget('full-page-widget',{
+        store: 'lampshoponline',
+        primaryClr: '#11CD6B',
+        layout:'fullWidth',
+        height: 300,
+        numReviews: 21
+      });
+      </script>
+    </div>
+  </div>
+</footer>
--- pages/led-fittings/index.php
+++ pages/led-fittings/index.php
@@ -1,39 +1,44 @@
 <?php
   $page = "LED Fittings";
   include('../../components/head-minimal.php');
-  $panel = "We recommend any of our LED Panels as they are all premium brands and the highest quality.";
  ?>
 <style media="screen">
-  .panel-body ul {list-style-type: none; margin-left: 0px; padding-left: 0px;}
+  .panel-body ul {margin-left: 0px; padding-left: 0px;}
   .panel-body ul li {color: #666;}
   .panel-body .btn {padding: 10px;
     margin: 10px 0px 0px 10px;
     font-size: 19px;
 }
 </style>
-<div class="col-sm-10 col-sm-offset-1" style="margin-top: 20px;">
-  <div class="panel panel-default" style="background-color: #ffffff; overflow: hidden;">
-    <div class="col-sm-4" style="margin: 0px; padding: 0px;">
-      <div class="panel-title" style="background-color: orange; color: #ffffff;">
-        <h2 style="margin: 0px; padding: 5px;">Most Popular</h2>
+<div class="col-sm-10 col-sm-offset-1">
+  <div class="col-sm-8" style="margin-top: 20px;">
+    <div class="panel panel-default" style="background-color: #ffffff; overflow: hidden;">
+      <div class="col-sm-4" style="margin: 0px; padding: 0px;">
+        <div class="panel-title" style="background-color: orange; color: #ffffff;">
+          <h2 style="margin: 0px; padding: 5px;">Most Popular</h2>
+        </div>
+        <img src="https://www.lampshoponline.com/media/catalog/category/Non-Corrosive_Batten_BS.jpg" alt="" style="width: 100%;">
       </div>
-      <img src="https://www.lampshoponline.com/media/catalog/category/Non-Corrosive_Batten_BS.jpg" alt="" style="width: 100%;">
-    </div>
-    <div class="col-sm-8" style="margin: 0px; padding: 0px;">
-      <div class="panel-title" style="background-color: orange; color: #ffffff;">
-        <h2 style="margin: 0px; padding: 5px;">BriteSource LED Luminaires - IP65</h2>
-      </div>
-      <div class="panel-body">
-        <ul style="font-size: 24px;">
-          <li>Non-Corrosive Fitting Complete With LED Boards</li>
-          <li>Complete Kit Ready To Install</li>
-          <li>5 Year Warranty</li>
-          <li><a href="#" class="btn btn-warning">Browse Category</a></li>
-        </ul>
+      <div class="col-sm-8" style="margin: 0px; padding: 0px;">
+        <div class="panel-title" style="background-color: orange; color: #ffffff;">
+          <h2 style="margin: 0px; padding: 5px;">BriteSource LED Luminaires - IP65</h2>
+        </div>
+        <div class="panel-body">
+          <ul style="font-size: 20px;">
+            <li>Non-Corrosive Fitting Complete With LED Boards</li>
+            <li>Complete Kit Ready To Install Within Minutes</li>
+            <li>5 Year Warranty With This LED Product</li>
+            <a href="https://www.lampshoponline.com/led/led-ready-fittings/led-luminaires-ip65.html" class="btn btn-warning">Browse Category</a>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
+  <div class="col-sm-4">
+    <img src="images/application.png" alt="LED Fitting Application" style="width: 100%; margin: 10px; border: 4px solid #333333;">
+  </div>
 </div>
+
 
 <div class="col-sm-10 col-sm-offset-1">
   <div class="col-sm-6">
@@ -56,9 +61,9 @@
         <div class="panel-body">
           <ul style="font-size: 20px;">
             <li>Batten Body Without Cover With Optional LED Tubes</li>
-            <li>2 Piece Kit Ready To Install</li>
-            <li>3 Year Warranty</li>
-            <li><a href="#" class="btn btn-success">Browse Category</a></li>
+            <li>2 Piece Kit Ready To Install Within Minutes</li>
+            <li>3 Year Warranty With This LED Product</li>
+            <a href="https://www.lampshoponline.com/led/led-ready-fittings/led-ready-batten-fittings.html" class="btn btn-success">Browse Category</a>
           </ul>
         </div>
       </div>
@@ -78,9 +83,9 @@
         <div class="panel-body">
           <ul style="font-size: 20px;">
             <li>Non-Corrosive Body With Optional T8 LED Tubes</li>
-            <li>2 Piece Kit Ready To Install</li>
-            <li>3 Year Warranty</li>
-            <li><a href="#" class="btn btn-success">Browse Category</a></li>
+            <li>2 Piece Kit Ready To Install Within Minutes</li>
+            <li>3 Year Warranty With This LED Product</li>
+            <a href="https://www.lampshoponline.com/led/led-ready-fittings/led-ready-non-corrosive-ip65-fittings.html" class="btn btn-success">Browse Category</a>
           </ul>
         </div>
 
@@ -89,3 +94,6 @@
     </div>
   </div>
 </div>
+<?php
+  include('../../components/footer-minimal.php');
+ ?>
