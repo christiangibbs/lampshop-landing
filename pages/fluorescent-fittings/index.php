a41ef500e36f06fdca4176c4039f30bc23d4ba96
Christian Gibbs <christian@cjgsolutions.com>
2017-10-30 11:23:29 +0000

    Fluorescent Fittings complete

---
+++ pages/fluorescent-fittings/index.php
@@ -0,0 +1,151 @@
+<?php
+  $page = "Fluorescent Fittings";
+  include('../../components/head-minimal.php');
+ ?>
+<style media="screen">
+  .panel-body ul {margin-left: 0px; padding-left: 0px; list-style-type: none;}
+  .panel-body ul li {color: #666;}
+  .panel-body .btn {padding: 10px;
+    margin: 10px 0px 0px 10px;
+    font-size: 19px; }
+  .most-popular {text-align: center;}
+  .most-popular h3 {font-size: 16px;}
+  .most-popular img {width: 100%;}
+}
+</style>
+<div class="col-sm-10 col-sm-offset-1">
+  <div class="col-sm-12" style="margin-top: 20px;">
+    <div class="panel panel-default most-popular">
+      <div class="panel-title">
+        <h2>Most Popular</h2>
+      </div>
+      <div class="panel-body">
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>2x58W Batten (Twin)</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/t/w/twin_batten_1.jpg" alt="2x58W Batten (Twin)">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/batten-fittings/2x58w-5ft-t8-fluorescent-batten-fitting-flicker-free.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>1x36W Batten (Single)</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/b/a/batten_fitting_1_2.jpg" alt="1x36W Batten (Single)">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/batten-fittings/1x36w-4ft-t8-fluorescent-batten-fitting-flicker-free.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>1x58W Batten (Single)</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/b/a/batten_fitting_1_1.jpg" alt="1x58W Batten (Single)">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/batten-fittings/1x58w-5ft-t8-fluorescent-batten-fitting-flicker-free.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>1x70W 6ft Batten (Single)</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/t/w/twin_batten_2.jpg" alt="1x70W 6ft Batten (Single)">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/batten-fittings/1x70w-6ft-t8-fluorescent-batten-fitting-flicker-free.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>2x58W Non-Corrosive IP65</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/1/_/1__01262.png" alt="Twin 58W Non-Corrosive">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/t8-non-corrosive-fittings-240v/twin-58w-5ft-t8-ip65-rated-hf-240v-non-corrosive-fitting.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="panel panel-default">
+            <div class="panel-title">
+              <h3>1x58W Non-Corrosive IP65</h3>
+            </div>
+            <div class="panel-body">
+              <img src="https://www.lampshoponline.com/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/f/i/fitting__40117.png" alt="Single 58W Non-Corrosive">
+              <a href="https://www.lampshoponline.com/fittings/t8-tube-fittings/t8-non-corrosive-fittings-240v/single-58w-5ft-t8-ip65-rated-hf-240v-non-corrosive-fitting.html" class="btn btn-warning">View Product</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="col-sm-10 col-sm-offset-1">
+  <div class="col-sm-6">
+    <div class="panel panel-default" style="background-color: #ffffff; overflow: hidden;">
+      <div class="panel-title" style="background-color: #109A3B; color: #ffffff;">
+        <h2 style="margin: 0px; padding: 5px;">T8 Non Corrosive Fittings - 240V</h2>
+      </div>
+      <div class="col-sm-3" style="margin: 0px; padding: 0px;">
+        <div class="panel-title" style="background-color: orange; color: #ffffff;">
+        </div>
+        <img src="https://www.lampshoponline.com/media/catalog/category/non_corrosive_T8_Twin.jpg" alt="" style="width: 100%; -moz-transform: scaleX(-1);
+        -o-transform: scaleX(-1);
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+        filter: FlipH;
+        -ms-filter: "FlipH";">
+      </div>
+      <div class="col-sm-9" style="margin: 0px; padding: 0px;">
+
+        <div class="panel-body">
+          <ul style="font-size: 20px;">
+            <li>Waterproof IP65 Fitting</li>
+            <li>Quality Product At Trade Prices</li>
+            <li>Perfect For Car Park Or Similar Applications</li>
+            <a href="https://www.lampshoponline.com/led/led-ready-fittings/led-ready-non-corrosive-ip65-fittings.html" class="btn btn-success">Browse Category</a>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="panel panel-default" style="background-color: #ffffff; overflow: hidden;">
+      <div class="panel-title" style="background-color: #109A3B; color: #ffffff;">
+        <h2 style="margin: 0px; padding: 5px;">Batten Fittings</h2>
+      </div>
+      <div class="col-sm-3" style="margin: 0px; padding: 0px;">
+        <img src="https://www.lampshoponline.com/media/catalog/category/batten_fitting.png" alt="" style="width: 100%;">
+      </div>
+      <div class="col-sm-9" style="margin: 0px; padding: 0px;">
+        <div class="panel-body">
+          <ul style="font-size: 20px;">
+            <li>High Frequency Instant Start Fitting</li>
+            <li>Quality Product At Trade Prices</li>
+            <li>Powder Coated Body</li>
+            <a href="https://www.lampshoponline.com/led/led-ready-fittings/led-ready-batten-fittings.html" class="btn btn-success">Browse Category</a>
+          </ul>
+        </div>
+
+
+      </div>
+
+    </div>
+  </div>
+</div>
+<?php
+  include('../../components/footer-minimal.php');
+ ?>
